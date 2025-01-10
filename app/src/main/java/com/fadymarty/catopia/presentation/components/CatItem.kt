cd0cdf5e688628fed4feabcc8dbe58aab3c0033b
fadyMarty <antoshka1234098@gmail.com>
2025-01-10 19:00:00 +0400

    1.0.1

--- app/src/main/java/com/fadymarty/catopia/presentation/components/CatItem.kt
+++ app/src/main/java/com/fadymarty/catopia/presentation/components/CatItem.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import coil3.compose.SubcomposeAsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
